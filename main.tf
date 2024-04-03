fa814479a2b2ba8c3c83fe6bc028abc4fca878bd
purna <purnaraomba@gmail.com>
2024-04-03 19:47:24 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_launch_template" "main" {
 
   vpc_security_group_ids = [aws_security_group.main.id]
 
+
   tag_specifications {
     resource_type = "instance"
 
@@ -36,6 +37,7 @@ resource "aws_autoscaling_group" "main" {
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.main.id
@@ -90,3 +92,46 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 5
+    timeout = 4
+  }
+  tags = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  )
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = local.dns_name
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.alb_dns_name]
+}
+
+resource "aws_lb_listener_rule" "listener" {
+  listener_arn = var.listener_arn
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = local.dns_name]
+    }
+  }
+}
+
