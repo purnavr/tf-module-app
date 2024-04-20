5e32b6683b6bd050b85faa241258697d6fbf76cd
purna <purnaraomba@gmail.com>
2024-04-20 10:09:26 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -50,17 +50,17 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
-#resource "aws_autoscaling_policy" "asg-cpu-rule" {
-#  name                   = "CPUUtilizationDetect"
-#  autoscaling_group_name = aws_autoscaling_group.main.name
-#  policy_type = "TargetTrackingScaling"
-#  target_tracking_configuration {
-#    predefined_metric_specification   {
-#      predefined_metric_type = "ASGAverageCPUUtilization"
-#    }
-#    target_value = 40.0
-#  }
-#}
+resource "aws_autoscaling_policy" "asg-cpu-rule" {
+  name                   = "CPUUtilizationDetect"
+  autoscaling_group_name = aws_autoscaling_group.main.name
+  policy_type = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification   {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 40.0
+  }
+}
 
 
 resource "aws_security_group" "main" {
